76e7f720736160788830048676f2374ed0722896
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-09-17 05:25:12 +0100

    backend code commit by ENGR. IPAYE

--- app/schema.py
+++ app/schema.py
@@ -2,12 +2,12 @@ from pydantic import BaseModel
 from typing import Optional
 from datetime import datetime
 
+
 class ChatMessage(BaseModel):
     type: str
     username: str
     content: Optional[str] = None
     timestamp: Optional[datetime] = None
-
 
     class Config:
         orm_mode = True
